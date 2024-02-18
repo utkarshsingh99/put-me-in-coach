79b2b259dc06759a788abbb3cfe3b143afa89969
Peregriney <45464810+Peregriney@users.noreply.github.com>
2024-02-18 08:19:44 -0800

    Update index.py
    text updated

--- reflex/webapp/webapp/pages/index.py
+++ reflex/webapp/webapp/pages/index.py
@@ -15,9 +15,9 @@ def index() -> rx.Component:
     """
     return rx.chakra.vstack(
         rx.chakra.heading("Put me in, coach!", font_size="3em"),
-        rx.chakra.text("Identify problems with your basketball skills"),
+        rx.chakra.text("Identify problems with your athletic form"),
         rx.chakra.text(
-            "Some more things about this project...",
+            "From your free throw shot to your overhead press, make sure that you are performing sport and athletic movements the right way. Track your workout history, watch tutorials, and get live AI-generated personal training to coach you on how you're moving.",
             # rx.chakra.code("{your_app}/pages/dashboard.py"),
         ),
     )
