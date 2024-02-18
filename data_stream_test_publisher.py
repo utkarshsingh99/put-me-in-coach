176d39fb7a5905f331de18297ba06692d5e748f4
AndChen153 <achen1192@gmail.com>
2024-02-18 14:51:51 +0000

    working on start stop

--- data_stream_test_publisher.py
+++ data_stream_test_publisher.py
@@ -1,31 +1,6 @@
 # Import package
 import paho.mqtt.client as mqtt
 
-# Define Variables
-MQTT_HOST = "192.168.137.183"
-MQTT_PORT = 1883
-MQTT_KEEPALIVE_INTERVAL = 5
-MQTT_TOPIC = "mqtt/rpi"
-MQTT_MSG = "Hello MQTT"
-
-# Define on_connect event Handler
-def on_connect(mosq, obj, rc):
-	print ("Connected to MQTT Broker")
-
-# Define on_publish event Handler
-def on_publish(client, userdata, mid):
-	print ("Message Published...")
-
-# Initiate MQTT Client
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
-
-# Register Event Handlers
-mqttc.on_publish = on_publish
-mqttc.on_connect = on_connect
-
-# Connect with MQTT Broker
-mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
-
 try:
 	# Publish message to MQTT Topic
 	count = 1
